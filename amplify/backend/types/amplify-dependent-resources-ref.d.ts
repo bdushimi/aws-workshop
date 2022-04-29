729bf5296be0eead43692b7109906d0ab1cd47aa
bdushimi <lespharaons@hotmail.com>
2022-04-29 18:52:35 +0200

    cleaup due to unkown error source

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "reactamplifiedtodo": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
